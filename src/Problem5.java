a6996ce6330197a3b03ee7fdcca8e9c6cfdb9bb3
Akshay-coder17 <akshayroxx8@gmail.com>
2026-04-15 11:16:01 +0530

    P5: Linear and Binary Search with Occurrence Counting

---
+++ src/Problem5.java
@@ -0,0 +1,97 @@
+import java.util.*;
+
+public class Problem5 {
+
+    static int linearSearchFirst(String[] arr, String target) {
+        int comparisons = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            comparisons++;
+            if (arr[i].equals(target)) {
+                System.out.println("Linear First Found at index: " + i);
+                System.out.println("Comparisons: " + comparisons);
+                return i;
+            }
+        }
+
+        System.out.println("Not found (Linear)");
+        return -1;
+    }
+
+    static int linearSearchLast(String[] arr, String target) {
+        int comparisons = 0;
+        int index = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            comparisons++;
+            if (arr[i].equals(target)) {
+                index = i;
+            }
+        }
+
+        System.out.println("Linear Last Found at index: " + index);
+        System.out.println("Comparisons: " + comparisons);
+        return index;
+    }
+
+    static int binarySearch(String[] arr, String target) {
+        int low = 0, high = arr.length - 1;
+        int comparisons = 0;
+
+        while (low <= high) {
+            comparisons++;
+            int mid = (low + high) / 2;
+
+            int cmp = arr[mid].compareTo(target);
+
+            if (cmp == 0) {
+                System.out.println("Binary Found at index: " + mid);
+                System.out.println("Comparisons: " + comparisons);
+                return mid;
+            } else if (cmp < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        System.out.println("Not found (Binary)");
+        return -1;
+    }
+
+    static int countOccurrences(String[] arr, String target, int index) {
+        if (index == -1) return 0;
+
+        int count = 1;
+
+        int left = index - 1;
+        while (left >= 0 && arr[left].equals(target)) {
+            count++;
+            left--;
+        }
+
+        int right = index + 1;
+        while (right < arr.length && arr[right].equals(target)) {
+            count++;
+            right++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        String[] logs = {"accB", "accA", "accB", "accC"};
+
+        linearSearchFirst(logs, "accB");
+        linearSearchLast(logs, "accB");
+
+        Arrays.sort(logs);
+        System.out.println("Sorted Logs: " + Arrays.toString(logs));
+
+        int index = binarySearch(logs, "accB");
+
+        int count = countOccurrences(logs, "accB", index);
+        System.out.println("Total occurrences: " + count);
+    }
+}
